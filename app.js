43fc43491d5d99d851436dbed89cf06c407c7d2e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-05-29 13:36:30 -0700

    created folder utlis with several js files

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const btn = get(".btn")
+
+const showUser = async () => {
+    // get user from api
+    const person = await getUser()
+    displayUser(person)
+
+    // display user
+}
+
+window.addEventListener("DOMContentLoaded", showUser)
+btn.addEventListener("click", showUser)
